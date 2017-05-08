5e56c7dcc0a864aef33ffe51c9edd6a0373cf86b
Ivan Vasilevich <ivanvbesarab@gmail.com>
2017-05-08 10:20:00 +0300

    constants file added set source string into userDefaults from firsVC

---
+++ SZL2EncryptTemplate/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SZL2EncryptTemplate
+//
+//  Created by Ivan Vasilevich on 5/8/17.
+//  Copyright © 2017 Smoosh Labs. All rights reserved.
+//
+
+import UIKit
+
+let kEncryptSourceString = "kEncryptSourceString"
--- SZL2EncryptTemplate/FirstViewController.swift
+++ SZL2EncryptTemplate/FirstViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func encryptContentOfTextField() {
+        UserDefaults.standard.set("abraKadabra", forKey: kEncryptSourceString)
     }
-
 
 }
 
